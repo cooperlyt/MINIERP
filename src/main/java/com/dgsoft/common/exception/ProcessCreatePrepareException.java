c5804aa5874884546fe247faaf67b97df69d9b48
cooper_lyt <lyt3131@163.com>
2014-01-05 01:46:08 +0800

    store change in complete

--- src/main/java/com/dgsoft/common/exception/ProcessCreatePrepareException.java
+++ src/main/java/com/dgsoft/common/exception/ProcessCreatePrepareException.java
@@ -1,11 +1,14 @@
 package com.dgsoft.common.exception;
 
+import org.jboss.seam.annotations.ApplicationException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: cooperlee
  * Date: 10/30/13
  * Time: 3:48 PM
  */
+@ApplicationException()
 public class ProcessCreatePrepareException extends IllegalArgumentException {
 
     public ProcessCreatePrepareException(){
