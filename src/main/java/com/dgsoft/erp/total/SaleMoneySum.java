d81684d6d8f11ece9e77e3103a852412af10d6d0
cooper-lyt <lyt3131@163.com>
2014-03-31 17:06:08 +0800

    2.0.3 begin

---
+++ src/main/java/com/dgsoft/erp/total/SaleMoneySum.java
@@ -0,0 +1,19 @@
+package com.dgsoft.erp.total;
+
+import org.jboss.seam.annotations.In;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cooperlee
+ * Date: 31/03/14
+ * Time: 16:44
+ */
+public class SaleMoneySum {
+
+
+    private BigDecimal
+
+}
