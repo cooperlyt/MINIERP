555ba98664e6327023c617d55a26bfa381c82cbe
cooper_lyt <lyt3131@163.com>
2014-05-08 22:23:43 +0800

    2.2.0 release

--- src/main/java/com/dgsoft/erp/business/order/OrderShipTaskHandle.java
+++ src/main/java/com/dgsoft/erp/business/order/OrderShipTaskHandle.java
@@ -39,25 +39,27 @@ public abstract class OrderShipTaskHandle extends OrderTaskHandle {
         orderHome.getInstance().setResReceived(!orderHome.isHaveShip());
         orderHome.getInstance().setAllShipDate(shipDate);
 
-        AccountOper accountOper;
-        if (orderHome.getInstance().getPayType().equals(CustomerOrder.OrderPayType.PAY_FIRST)) {
-            accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
-                    shipDate, BigDecimal.ZERO, orderHome.getInstance().getMoney(), BigDecimal.ZERO, BigDecimal.ZERO);
-        } else if (orderHome.getInstance().getPayType().equals(CustomerOrder.OrderPayType.EXPRESS_PROXY)) {
-            accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
-                    shipDate, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, orderHome.getInstance().getMoney());
-        } else {
-            BigDecimal canUseAvanceMoney = orderHome.getInstance().getCustomer().getAdvanceMoney().
-                    subtract(CustomerMoneyTool.instance().getOrderAdvance(orderHome.getInstance().getCustomer().getId()));
-            if (canUseAvanceMoney.compareTo(orderHome.getInstance().getMoney()) > 0) {
-                canUseAvanceMoney = orderHome.getInstance().getMoney();
+        if (orderHome.getInstance().getMoney().compareTo(BigDecimal.ZERO) != 0) {
+            AccountOper accountOper;
+            if (orderHome.getInstance().getPayType().equals(CustomerOrder.OrderPayType.PAY_FIRST)) {
+                accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
+                        shipDate, BigDecimal.ZERO, orderHome.getInstance().getMoney(), BigDecimal.ZERO, BigDecimal.ZERO);
+            } else if (orderHome.getInstance().getPayType().equals(CustomerOrder.OrderPayType.EXPRESS_PROXY)) {
+                accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
+                        shipDate, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, orderHome.getInstance().getMoney());
+            } else {
+                BigDecimal canUseAvanceMoney = orderHome.getInstance().getCustomer().getAdvanceMoney().
+                        subtract(CustomerMoneyTool.instance().getOrderAdvance(orderHome.getInstance().getCustomer().getId()));
+                if (canUseAvanceMoney.compareTo(orderHome.getInstance().getMoney()) > 0) {
+                    canUseAvanceMoney = orderHome.getInstance().getMoney();
+                }
+                accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
+                        shipDate, BigDecimal.ZERO, canUseAvanceMoney, orderHome.getInstance().getMoney().subtract(canUseAvanceMoney), BigDecimal.ZERO);
             }
-            accountOper = new AccountOper(orderHome.getInstance(), orderHome.getInstance().getOrderEmp(), AccountOper.AccountOperType.ORDER_PAY,
-                    shipDate, BigDecimal.ZERO, canUseAvanceMoney, orderHome.getInstance().getMoney().subtract(canUseAvanceMoney), BigDecimal.ZERO);
-        }
 
-        accountOper.calcCustomerMoney();
-        orderHome.getInstance().getAccountOpers().add(accountOper);
+            accountOper.calcCustomerMoney();
+            orderHome.getInstance().getAccountOpers().add(accountOper);
+        }
     }
 
 }
