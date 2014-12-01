9ba6759aae32df6426c7e1b72062f738967a4374
cooper_lyt <lyt3131@163.com>
2014-12-01 22:17:50 +0800

    2.3.2 release

--- src/main/webapp/resources/collapsible-subtable.js
+++ src/main/webapp/resources/collapsible-subtable.js
@@ -2,7 +2,7 @@
 
     rf.ui = rf.ui || {};
 
-    rf.ui.CollapsibleSubTable = function(id, f, options) {
+    rf.ui.CollapsibleSubTable = function (id, f, options) {
         this.id = id;
         this.options = $.extend(this.options, options || {});
         this.stateInput = options.stateInput;
@@ -12,13 +12,13 @@
         this.formId = f;
         this.isNested = options.isNested;
 
-        if (! this.isNested) {
+        if (!this.isNested) {
             var self = this;
             var tbody = $(document.getElementById(this.id)).parent();
-            tbody.find(".rf-dt-c-srt").each(function() {
+            tbody.find(".rf-dt-c-srt").each(function () {
                 $(this).bind("click", {sortHandle: this}, $.proxy(self.sortHandler, self));
             });
-            tbody.find(".rf-dt-flt-i").each(function() {
+            tbody.find(".rf-dt-flt-i").each(function () {
                 $(this).bind("blur", {filterHandle: this}, $.proxy(self.filterHandler, self));
             });
 
@@ -28,25 +28,25 @@
     };
 
     $.extend(rf.ui.CollapsibleSubTable, {
-            MODE_AJAX: "ajax",
-            MODE_SRV: "server",
-            MODE_CLNT: "client",
-            collapse: 0,
-            expand: 1,
-            SORTING: "rich:sorting",
-            FILTERING: "rich:filtering"
-        });
+        MODE_AJAX: "ajax",
+        MODE_SRV: "server",
+        MODE_CLNT: "client",
+        collapse: 0,
+        expand: 1,
+        SORTING: "rich:sorting",
+        FILTERING: "rich:filtering"
+    });
 
     rf.BaseComponent.extend(rf.ui.CollapsibleSubTable);
     var $super = rf.ui.CollapsibleSubTable.$super;
 
     $.extend(rf.ui.CollapsibleSubTable.prototype, (function () {
 
-        var invoke = function(event, attributes) {
-            rf.ajax(this.id, event, {"parameters" : attributes});
+        var invoke = function (event, attributes) {
+            rf.ajax(this.id, event, {"parameters": attributes});
         };
 
-        var createParameters = function(type, id, arg1, arg2) {
+        var createParameters = function (type, id, arg1, arg2) {
             var parameters = {};
             var key = this.id + type;
             parameters[key] = (id + ":" + (arg1 || "") + ":" + arg2);
@@ -60,50 +60,38 @@
             return parameters;
         };
 
-        var element = function() {
-            if (! this.isNested) {
+        var element = function () {
+            if (!this.isNested) {
                 //use parent tbody as parent dom elem
                 return $(document.getElementById(this.id)).parent();
             } else {
-                var regex = new RegExp(this.id + "\\:\\d+\\:b");
-                return $(document.getElementById(this.id)).parent().find("tr[style!='display: none;']").filter(function() {
+                var regex = new RegExp("^" + this.id + "\\:\\d+\\:b$");
+                return $(document.getElementById(this.id)).parent().find("tr").filter(function () {
                     return this.id.match(regex);
                 });
             }
         };
 
-        var stateInputElem = function() {
+        var stateInputElem = function () {
             return $(document.getElementById(this.stateInput));
         };
 
-        var optionsInputElem = function() {
+        var optionsInputElem = function () {
             return $(document.getElementById(this.optionsInput));
         };
 
-        var ajax = function(e, options) {
+        var ajax = function (e, options) {
             this.__switchState();
             rf.ajax(this.id, e, options);
         };
 
-        var server = function(options) {
+        var server = function (options) {
             this.__switchState();
             $(document.getElementById(this.formId)).submit();
         };
 
-        var client = function(options) {
+        var client = function (options) {
             if (this.isExpanded()) {
-                if(this.isNested){
-                    var regex = new RegExp(this.id + "\\:\\d+\\:\\w+");
-
-                    $(document.getElementById(this.id)).parent().find("tr[style='display: none;']").filter(function() {
-                        return this.id.match(regex);
-                    }).each(function(){
-                        alert(this.id);
-                        if(this.rf) {
-                            this.rf.component.collapse();
-                        }
-                    });
-                }
                 this.collapse(options);
             } else {
                 this.expand(options);
@@ -115,15 +103,15 @@
 
             name: "CollapsibleSubTable",
 
-            sort: function(columnId, direction, isClear) {
+            sort: function (columnId, direction, isClear) {
                 invoke.call(this, null, createParameters.call(this, rf.ui.CollapsibleSubTable.SORTING, columnId, direction, isClear));
             },
 
-            clearSorting: function() {
+            clearSorting: function () {
                 this.sort("", "", true);
             },
 
-            sortHandler: function(event) {
+            sortHandler: function (event) {
                 var sortHandle = $(event.data.sortHandle);
                 var button = sortHandle.find('.rf-dt-srt-btn');
                 var columnId = button.data('columnid');
@@ -131,22 +119,22 @@
                 this.sort(columnId, sortOrder, false);
             },
 
-            filter: function(columnId, filterValue, isClear) {
+            filter: function (columnId, filterValue, isClear) {
                 invoke.call(this, null, createParameters.call(this, rf.ui.CollapsibleSubTable.FILTERING, columnId, filterValue, isClear));
             },
 
-            clearFiltering: function() {
+            clearFiltering: function () {
                 this.filter("", "", true);
             },
 
-            filterHandler: function(event) {
+            filterHandler: function (event) {
                 var filterHandle = $(event.data.filterHandle);
                 var columnId = filterHandle.data('columnid');
                 var filterValue = filterHandle.val();
                 this.filter(columnId, filterValue, false);
             },
 
-            switchState: function(e, options) {
+            switchState: function (e, options) {
                 if (this.expandMode == rf.ui.CollapsibleSubTable.MODE_AJAX) {
                     ajax.call(this, e, this.eventOptions, options);
                 } else if (this.expandMode == rf.ui.CollapsibleSubTable.MODE_SRV) {
@@ -156,41 +144,69 @@
                 }
             },
 
-            collapse: function(options) {
+            collapse: function (options) {
+
+
+                if (this.isNested) {
+                    var expandedTogglerRegex = new RegExp("^" + this.id + "\\:\\d+\\:\\w+\\:expanded$");
+                    var collapseTogglerRegex = new RegExp("^" + this.id + "\\:\\d+\\:\\w+\\:collapsed$");
+                    var subtableRegex = new RegExp("^" + this.id + "\\:\\d+\\:\\w+$");
+                    $(document.getElementById(this.id)).parent().find("tr[style='display: none;']").filter(function () {
+                        return this.id.match(subtableRegex);
+                    }).each(function () {
+                        if (this.rf) {
+                            if (this.rf.component.isExpanded) {
+                                $(document.getElementById(this.id)).parent().find(".rf-csttg-exp").filter(function () {
+                                    return this.id.match(expandedTogglerRegex);
+                                }).each(function () {
+                                    $(this).hide();
+                                });
+                                $(document.getElementById(this.id)).parent().find(".rf-csttg-colps").filter(function () {
+                                    return this.id.match(collapseTogglerRegex);
+                                }).each(function () {
+                                    $(this).show();
+                                });
+                                this.rf.component.collapse();
+                            }
+                        }
+                    });
+                }
+
+
                 this.setState(rf.ui.CollapsibleSubTable.collapse);
                 element.call(this).hide();
             },
 
-            expand: function(options) {
+            expand: function (options) {
                 this.setState(rf.ui.CollapsibleSubTable.expand);
                 element.call(this).show();
             },
 
-            isExpanded: function() {
+            isExpanded: function () {
                 return (parseInt(this.getState()) == rf.ui.CollapsibleSubTable.expand);
             },
 
-            __switchState: function(options) {
+            __switchState: function (options) {
                 var state = this.isExpanded() ? rf.ui.CollapsibleSubTable.collapse : rf.ui.CollapsibleSubTable.expand;
                 this.setState(state);
             },
 
-            getState: function() {
+            getState: function () {
                 return stateInputElem.call(this).val();
             },
 
-            setState: function(state) {
+            setState: function (state) {
                 stateInputElem.call(this).val(state)
             },
 
-            setOption: function(option) {
+            setOption: function (option) {
                 optionsInputElem.call(this).val(option);
             },
 
-            getMode: function() {
+            getMode: function () {
                 return this.expandMode;
             },
-            destroy: function() {
+            destroy: function () {
                 $super.destroy.call(this);
             }
         };
